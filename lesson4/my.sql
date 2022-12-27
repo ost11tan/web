148f997d4848651078b0aefd0f75832f657b82a6
tati <ost11tan@gmail.com>
2022-12-27 20:01:41 +0300

    Задание 2,исправленное

--- lesson4/my.sql
+++ lesson4/my.sql
@@ -1,6 +1,6 @@
 
 -- create
-CREATE TABLE EMPLOYEE (
+CREATE TABLE employee (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   age INTEGER NOT NULL,
@@ -8,14 +8,14 @@ CREATE TABLE EMPLOYEE (
 );
 
 -- insert
-INSERT INTO EMPLOYEE VALUES (0001, 'Татьяна', '27','Нальчик');
-INSERT INTO EMPLOYEE VALUES (0002, 'Антон', '29','Краснодар');
-INSERT INTO EMPLOYEE VALUES (0003, 'Андрей', '42','Москва');
-INSERT INTO EMPLOYEE VALUES (0004, 'Дмитрий', '34','Ковров');
-INSERT INTO EMPLOYEE VALUES (0005, 'Наталья', '32','Тюмень');
-INSERT INTO EMPLOYEE VALUES (0006, 'Милиса', '30','Уфа');
-INSERT INTO EMPLOYEE VALUES (0007, 'Константин', '28','Москва');
-INSERT INTO EMPLOYEE VALUES (0009, 'Александр', '26','Санкт-Петербург');
-INSERT INTO EMPLOYEE VALUES (0010, 'Елена', '20','Москва');
+INSERT INTO employee VALUES (0001, 'Татьяна', 27,'Нальчик');
+INSERT INTO employee VALUES (0002, 'Антон', 29,'Краснодар');
+INSERT INTO employee VALUES (0003, 'Андрей', 42,'Москва');
+INSERT INTO employee VALUES (0004, 'Дмитрий', 34,'Ковров');
+INSERT INTO employee VALUES (0005, 'Наталья', 32,'Тюмень');
+INSERT INTO employee VALUES (0006, 'Милиса', 30,'Уфа');
+INSERT INTO employee VALUES (0007, 'Константин', 28,'Москва');
+INSERT INTO employee VALUES (0009, 'Александр', 26,'Санкт-Петербург');
+INSERT INTO employee VALUES (0010, 'Елена', 20,'Москва');
 -- fetch 
-SELECT name FROM EMPLOYEE WHERE address = 'Москва' AND age < 30 AND age >= 18;
+SELECT name FROM employee WHERE address = 'Москва' AND age < 30 AND age >= 18;
